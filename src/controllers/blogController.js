5616633610f66d78f8505214eaac220ae15127da
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-21 15:47:28 +0530

    API GET BLOG

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,9 +1,26 @@
 const { count } = require("console")
 const blogModel = require("../models/blogModel")
+const authorModel = require("../models/autherModel")
 
 const createBlog = async function(req, res) {
-    let data = req.body
-    let saveData = await blogModel.create(data)
-    res.status(201).send({ saveData })
+    try {
+        let blog = req.body
+        let authorId = await authorModel.find().select({ _id: 1 })
+        let authorIdArr = authorId.map((obj) => { return obj._id.toString() }) // Array formation of all the author id from our Author Collections
+        console.log(authorIdArr)
+
+        if (blog.authorId !== undefined) {
+            return res.status(400).send({ status: false, msg: "Invale Request" })
+        }
+        if (authorIdArr.includes(blog.authorId)) {
+            return res.status(400).send({ status: false, msg: "Invale Request" })
+        }
+
+        let saveData = await blogModel.create(blog)
+        res.status(201).send({ status: true, msg: saveData })
+
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+    }
 }
 module.exports.createBlog = createBlog
