944381a9a08fdca135369b1c1f912f1f2529e6e7
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-21 23:10:13 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const autherController = require('../controllers/autherController')
 const blogController = require('../controllers/blogController.js')
 const getController = require('../controllers/getController.js')
-
-
+const updateController = require('../controllers/updateController')
+const deleteController = require('../controllers/deleteController.js')
 
 router.post("/author", autherController.createAuther)
 
@@ -14,5 +14,10 @@ router.get("/get/Blogs", getController.getBlog)
 
 router.get("/blogs", getController.blogs)
 
+router.put("/blogs/:blogId", updateController.updateBlog)
+
+router.delete("/blogs/:blogId", deleteController.deleteBlogByParams)
+
+router.delete("/blogs", deleteController.deleteBlogByQuery)
 
 module.exports = router;
