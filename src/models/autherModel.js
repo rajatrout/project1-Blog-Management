b3aa6dc20c15800bf0b0efb8076c5ffdf642022a
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-22 11:37:26 +0530

    Email validation

--- src/models/autherModel.js
+++ src/models/autherModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const autherSchema = new mongoose.Schema({
     fname: {
@@ -17,7 +18,12 @@ const autherSchema = new mongoose.Schema({
     emailId: {
         type: String,
         unique: true,
-        required: true
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email is invalid")
+            }
+        }
     },
     password: {
         type: String,
